ecfd7007f21098274090f8be7fa9f956b6ee124a
Glitch (glitch-hello-website) <none>
2023-06-20 18:35:40 +0000

    🐩🚔 Checkpoint ./index.html:2209215/6958 ./script.js:2209215/5612 ./.glitch-assets:2209215/427

--- script.js
+++ script.js
@@ -1,34 +1,85 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+// plain math
+const { abs, sin, cos, atan2, PI, log, sqrt } = Math;
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+// vector math
+const sub = (v1, v2) => ({ x: v1.x - v2.x, y: v1.y - v2.y, z: v1.z - v2.z });
+const muls = (v, s) => ({ x: v.x * s, y: v.y * s, z: v.z * s });
+const dot = (v1, v2) => v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+const mag = (v) => sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2);
+const unit = (v, m = mag(v)) => ({ x: v.x / m, y: v.y / m, z: v.z / m });
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
+const reflect = (ray, normal) => {
+  ray = unit(ray);
+  normal = unit(normal);
+  return sub(muls(normal, 2 * dot(ray, normal)), ray);
+};
+
+const map = (v, ds, de, ts, te) => {
+  const d = de - ds;
+  if (d === 0) return ts;
+  return ts + ((v - ds) * (te - ts)) / d;
+};
+
+const constrain = (v, m, M) => {
+  if (m > M) return constrain(v, M, m);
+  return v > M ? M : v < m ? m : v;
+};
+
+const w = 500;
+const h = w;
+cvs.width = cvs.height = w;
+const ctx = cvs.getContext(`2d`);
+const im = new Image();
+im.crossOrigin = `anonymous`;
+im.src = `https://cdn.glitch.global/6f093c76-7f96-4f52-94dd-2b1647bfb115/ALPSMLC30_N048W120_DSM.png?v=1687285523873`;
+
+// hill shader
+function hillShade(evt) {
+  ctx.drawImage(im, 0, 0, w, h);
+  const imageData = ctx.getImageData(0, 0, w, h);
+  const pixels = imageData.data;
+  const shaded = ctx.createImageData(w, h);
+
+  const getElevation = (x, y) => {
+    x = x < 0 ? 0 : x >= w ? w - 1 : x;
+    y = y < 0 ? 0 : y >= h ? h - 1 : y;
+    return pixels[4 * (x + y * w)];
   };
+
+  const { width, height } = cvs.getBoundingClientRect();
+  const w2 = width / 2;
+  const h2 = height / 2;
+  let x = (100 * (evt.offsetX - w2)) / w2;
+  let y = (100 * (evt.offsetY - h2)) / h2;
+  const light = { x: -x, y: -y, z: 1 };
+
+  let max = 0;
+
+  for (let x = 0; x < w; x++) {
+    for (let y = 0; y < h; y++) {
+      const i = 4 * (x + y * w);
+
+      const a = getElevation(x - 1, y);
+      const b = getElevation(x + 1, y);
+      const c = getElevation(x, y - 1);
+      const d = getElevation(x, y + 1);
+      const n = unit({ x: a - b, y: c - d, z: 2 });
+
+      // compute illumination
+      const reflection = unit(reflect(light, n));
+      const z = reflection.z ** 0.25;
+      const e = constrain(map(z, 0, 1, 0, 255), 0, 255);
+
+      // noise reduction using the alpha channel
+      shaded.data[i + 0] = e;
+      shaded.data[i + 1] = e;
+      shaded.data[i + 2] = e;
+      shaded.data[i + 3] = 255;
+    }
+  }
+
+  ctx.putImageData(shaded, 0, 0);
 }
 
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
+cvs.addEventListener(`mousemove`, hillShade);
+hillShade({ offsetX: 0, offsetY: 0 });
