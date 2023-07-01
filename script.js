cefe801650b0b1510d8da8f09bd0905b5e2cc1f5
Glitch (glitch-hello-website) <none>
2023-07-01 05:48:26 +0000

    📌🏕 Checkpoint ./.glitch-assets:2209215/43 ./script.js:2209215/440

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ let isoMap;
 let normals;
 let hillShade = () => {};
 
-const SOURCE = `https://cdn.glitch.global/6f093c76-7f96-4f52-94dd-2b1647bfb115/ALPSMLC30_N048W125_DSM.120m.png?v=1688177542690`;
-const BGSOURCE = `https://cdn.glitch.global/6f093c76-7f96-4f52-94dd-2b1647bfb115/ALPSMLC30_N048W125_DSM.120m.png?v=1688177542690`;
+const SOURCE = `https://cdn.glitch.global/6f093c76-7f96-4f52-94dd-2b1647bfb115/ALPSMLC30_N048W124_DSM.120m.png?v=1688152031668`;
+const BGSOURCE = `https://cdn.glitch.global/6f093c76-7f96-4f52-94dd-2b1647bfb115/ALPSMLC30_N048W124_DSM.120m.png?v=1688152031668`;
 const bg = new Image();
 
 // Let's set up the main canvas using nicely big dimensions
